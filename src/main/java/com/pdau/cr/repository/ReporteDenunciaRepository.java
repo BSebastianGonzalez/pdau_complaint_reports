f02168bf4f9bf1104d1508f16b3dffd0a807c1f0
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-12-08 16:06:27 -0500

    feat: added RabbitMQ configuration

---
+++ src/main/java/com/pdau/cr/repository/ReporteDenunciaRepository.java
@@ -0,0 +1,7 @@
+package com.pdau.cr.repository;
+
+import com.pdau.cr.model.ReporteDenuncia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReporteDenunciaRepository extends JpaRepository<ReporteDenuncia, Long> {
+}
