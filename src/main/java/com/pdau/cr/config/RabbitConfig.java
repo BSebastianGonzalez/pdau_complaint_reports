1520d1ff5eb0056687190d6b82800a9e9ca9d5a9
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-12-08 16:43:46 -0500

    hotfix: fixed pom.xml due to uncompatibilities between dependencies

--- src/main/java/com/pdau/cr/config/RabbitConfig.java
+++ src/main/java/com/pdau/cr/config/RabbitConfig.java
@@ -5,7 +5,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,8 +35,8 @@ public class RabbitConfig {
     }
 
     @Bean
-    public JacksonJsonMessageConverter jackson2JsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
