f6081f399a59c3e031aa31a968b7516483588644
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-12-07 23:25:57 -0500

    first commit

---
+++ src/main/java/com/pdau/cr/ComplaintReportApplication.java
@@ -0,0 +1,13 @@
+package com.pdau.cr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdau/cr/ComplaintReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdau.cr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
