a6a38bbc4590f15b134b61e43340d21ac19e517a
lesg1151860 <luiseduardosg@ufps.edu.co>
2025-12-08 19:21:24 -0500

    feat: added consults about complaints for statistics

--- src/main/java/com/pdau/cr/repository/ReporteDenunciaRepository.java
+++ src/main/java/com/pdau/cr/repository/ReporteDenunciaRepository.java
@@ -2,6 +2,26 @@ package com.pdau.cr.repository;
 
 import com.pdau.cr.model.ReporteDenuncia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
 
 public interface ReporteDenunciaRepository extends JpaRepository<ReporteDenuncia, Long> {
+    // TOTAL
+    @Query("SELECT COUNT(r) FROM ReporteDenuncia r")
+    long totalDenuncias();
+
+    // POR ESTADO
+    @Query("SELECT r.estado, COUNT(r) FROM ReporteDenuncia r GROUP BY r.estado")
+    List<Object[]> totalPorEstado();
+
+    // POR FECHAS
+    @Query("SELECT COUNT(r) FROM ReporteDenuncia r WHERE r.fechaCreacion BETWEEN :inicio AND :fin")
+    long totalPorFechas(@Param("inicio") Date inicio,
+                        @Param("fin") Date fin);
+
+    // Todas para categorías
+    List<ReporteDenuncia> findAll();
 }
